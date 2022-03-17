836252b81b8a48c3f292e6976d84d49e281e364f
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2022-03-17 11:00:56 +0530

    Update Stream.py

--- Yukki/Plugins/Stream.py
+++ Yukki/Plugins/Stream.py
@@ -32,19 +32,7 @@ from Yukki.Utilities.youtube import (get_m3u8, get_yt_info_id,
 
 loop = asyncio.get_event_loop()
 
-__MODULE__ = "VideoCalls"
-__HELP__ = f"""
 
-/play [Reply to any Video] or [YT Link] or [Music Name]
-- Stream Video on Voice Chat
-
-**For Sudo User:-**
-
-/set_video_limit [Number of Chats]
-- Set a maximum Number of Chats allowed for Video Calls at a time.
-
-
-"""
 
 
 @app.on_callback_query(filters.regex(pattern=r"Yukki"))
