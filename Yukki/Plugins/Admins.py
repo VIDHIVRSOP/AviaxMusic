165622937385e76f771969f072f7dc20cab68ab5
Rahulchoudhary17 <73299986+Rahulchoudhary17@users.noreply.github.com>
2022-03-17 10:54:17 +0530

    Update Admins.py

--- Yukki/Plugins/Admins.py
+++ Yukki/Plugins/Admins.py
@@ -32,35 +32,6 @@ from Yukki.Utilities.youtube import get_m3u8, get_yt_info_id
 loop = asyncio.get_event_loop()
 
 
-__MODULE__ = "Voice Chat"
-__HELP__ = """
-
-
-/pause
-- Pause the playing music on voice chat.
-
-/resume
-- Resume the paused music on voice chat.
-
-/skip
-- Skip the current playing music on voice chat
-
-/end or /stop
-- Stop the playout.
-
-/queue
-- Check queue list.
-
-
-**Note:**
-Only for Sudo Users
-
-/activevc
-- Check active voice chats on bot.
-
-/activevideo
-- Check active video calls on bot.
-"""
 
 
 @app.on_message(
